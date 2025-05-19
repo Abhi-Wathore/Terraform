dd6604ac3941ef2e601db80687ace53d8f94f29c
Abhishek Wathore <abhishekbwathore@gmail.com>
2025-05-19 23:52:26 +0530

    module

---
+++ VPC/variable.tf
@@ -0,0 +1,22 @@
+# This file contains the variables used in the VPC module.
+# It defines the variables for the VPC module, including the region, CIDR block, and tags.
+# These variables are used to create the VPC and its associated resources.
+# The variables are defined with default values, but you can override them when you call the module.
+
+variable "vpc_name" {}
+
+variable "vpc_cidr" {}
+
+variable "pub_subnet_cidr" {}
+
+variable "pvt_subnet_cidr" {}
+
+variable "availability_zone" {}
+
+variable "route_cidr" {}
+
+variable "security_group_name" {}
+
+variable "pub_subnet_name" {}
+
+variable "pvt_subnet_name" {}
