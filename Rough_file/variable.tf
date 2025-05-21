f1bc2a881657aa0bb5d09bd078fb8b2f8c5dd799
Abhishek Wathore <abhishekbwathore@gmail.com>
2025-05-21 23:31:55 +0530

    Rough file for reffrence

--- Rough_file/variable.tf
+++ Rough_file/variable.tf
@@ -23,3 +23,35 @@ variable "availability_zone" {
     default     = "eu-north-1a"
   
 }
+
+
+variable "ami" {
+  description = "AMI ID for the EC2 instance"
+  type        = string
+}
+
+variable "instance_type" {
+  description = "Type of EC2 instance"
+  type        = string
+  default     = "t2.micro"
+}
+
+variable "key_name" {
+  description = "SSH key pair name"
+  type        = string
+}
+
+variable "instance_name" {
+  description = "Name tag for EC2 instances"
+  type        = string
+}
+
+variable "environment" {
+  description = "Environment tag for EC2 instances"
+  type        = string
+}
+
+variable "project_name" {
+  description = "Project name tag for EC2 instances"
+  type        = string
+}
